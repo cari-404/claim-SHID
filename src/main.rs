5d05a35a3593eb21a116ef9993c84247479892c0
cari-404 <135954797+cari-404@users.noreply.github.com>
2024-04-06 18:54:49 +0700

    Whats new In 1.1.0 : add modes

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,18 @@
 /*
-This Is a first version of get_vouchers_by_collections
+This Is a first version of save_voucher
 This version using api reqwest
-Whats new In 1.1.8 :
-Add csrftoken function
-restructure header
-Whats new In 1.1.7 :
-fix for windows 7 console
-Whats new In 1.1.6 :
-fix included ansicode on logs
-Whats new In 1.1.5 :
-Add function interactive_print
+Whats new In 1.1.0 :
+add modes
+Whats new In 1.0.0 :
+initial releases
 */
 
 use reqwest;
 use reqwest::ClientBuilder;
-use reqwest::Body;
 use serde_json;
-use serde_json::json;
+use serde::Serialize;
 use anyhow::Result;
 use reqwest::Version;
-use std::fs;
 use std::fs::File;
 use std::thread;
 use std::time::Duration as StdDuration;
@@ -27,11 +20,26 @@ use std::io::{self, Read, Write};
 use chrono::{Local, Duration, NaiveDateTime};
 use structopt::StructOpt;
 
+mod prepare;
+
 #[derive(Debug, StructOpt)]
 #[structopt(name = "Auto save voucher Shopee", about = "Make fast save from shopee.co.id")]
 struct Opt {
     #[structopt(short, long, help = "time to run checkout")]
     time: Option<String>, 
+}
+
+#[derive(Serialize)]
+struct SaveVoucherRequest {
+    voucher_promotionid: i64,
+	signature: String,
+	security_device_fingerprint: String,
+	signature_source: String,
+}
+
+enum Mode {
+    Normal,
+    Collection,
 }
 
 fn extract_csrftoken(cookie_string: &str) -> String {
@@ -45,25 +53,7 @@ fn extract_csrftoken(cookie_string: &str) -> String {
     csrftoken
 }
 
-async fn some_function(start: &str, end: &str, cookie_content: &str, selected_file: &str) -> Result<()> {
-
-	// Mengonversi nama akun menjadi format folder yang sesuai
-    let header_folder = format!("./header/{}/af-ac-enc-sz-token.txt", selected_file);
-	
-	// Membuat folder header jika belum ada
-    fs::create_dir_all(&format!("./header/{}", selected_file))?;
-
-    // Membuat file header jika belum ada
-    if !File::open(&header_folder).is_ok() {
-        let mut header_file = File::create(&header_folder)?;
-        // Isi file header dengan konten default atau kosong sesuai kebutuhan
-        header_file.write_all(b"ganti kode ini dengan sz-token valid")?;
-    }
-
-    // Baca isi file untuk header af-ac-enc-sz-token
-    let mut sz_token_content = String::new();
-    File::open(&header_folder)?.read_to_string(&mut sz_token_content)?;
-	println!("sz-token:{}", sz_token_content);
+async fn some_function(start: &str, end: &str, cookie_content: &str) -> Result<()> {
     let cookie_content_owned = cookie_content.to_string();
 
     // Pass the cloned String to extract_csrftoken
@@ -72,58 +62,76 @@ async fn some_function(start: &str, end: &str, cookie_content: &str, selected_fi
 	let csrftoken_string = csrftoken.to_string();
 	let start: i64 = start.trim().parse().expect("Input tidak valid");
 
-	let body_json = json!({
-	  "voucher_promotionid": start as i64,
-	  "signature": end.replace(char::is_whitespace, ""),
-	  "security_device_fingerprint": sz_token_content,
-	  "signature_source": "0"
-	});
+	let body_json = SaveVoucherRequest {
+	  voucher_promotionid: start as i64,
+	  signature: end.to_string(),
+	  security_device_fingerprint: String::new(),
+	  signature_source: 0.to_string(),
+	};
 	
     let body_str = serde_json::to_string(&body_json)?;
 
     println!("{}", body_str);
 	
 	let mut headers = reqwest::header::HeaderMap::new();
-
-	headers.insert("x-sap-access-f", reqwest::header::HeaderValue::from_static(""));
-	headers.insert("x-shopee-client-timezone", reqwest::header::HeaderValue::from_static("Asia/Jakarta"));
-	headers.insert("x-sap-access-t", reqwest::header::HeaderValue::from_static(""));
-	headers.insert("af-ac-enc-dat", reqwest::header::HeaderValue::from_static(""));
-	headers.insert("af-ac-enc-id", reqwest::header::HeaderValue::from_static(""));
-	headers.insert("x-csrftoken", reqwest::header::HeaderValue::from_str(&csrftoken_string)?);
-	headers.insert("user-agent", reqwest::header::HeaderValue::from_static("Android app Shopee appver=32010 app_type=1"));
-	headers.insert("x-api-source", reqwest::header::HeaderValue::from_static("rn"));
-	headers.insert("content-type", reqwest::header::HeaderValue::from_static("application/json"));
+	headers.insert("User-Agent", reqwest::header::HeaderValue::from_static("Android app Shopee appver=29313 app_type=1"));
 	headers.insert("accept", reqwest::header::HeaderValue::from_static("application/json"));
+	headers.insert("Content-Type", reqwest::header::HeaderValue::from_static("application/json"));
+	headers.insert("x-api-source", reqwest::header::HeaderValue::from_static("rn"));
 	headers.insert("if-none-match-", reqwest::header::HeaderValue::from_static("55b03-97d86fe6888b54a9c5bfa268cf3d922f"));
 	headers.insert("shopee_http_dns_mode", reqwest::header::HeaderValue::from_static("1"));
-	headers.insert("af-ac-enc-sz-token", reqwest::header::HeaderValue::from_str(&sz_token_content)?);
-	headers.insert("origin", reqwest::header::HeaderValue::from_static("https://shopee.co.id"));
-	headers.insert("referer", reqwest::header::HeaderValue::from_static("https://mall.shopee.co.id"));
-	headers.insert("accept-encoding", reqwest::header::HeaderValue::from_static("gzip, deflate"));
-
+	headers.insert("x-shopee-client-timezone", reqwest::header::HeaderValue::from_static("Asia/Jakarta"));
+	headers.insert("af-ac-enc-dat", reqwest::header::HeaderValue::from_static(""));
+	headers.insert("af-ac-enc-id", reqwest::header::HeaderValue::from_static(""));
+	headers.insert("x-sap-access-t", reqwest::header::HeaderValue::from_static(""));
+	headers.insert("x-sap-access-f", reqwest::header::HeaderValue::from_static(""));
+	headers.insert("referer", reqwest::header::HeaderValue::from_static("https://mall.shopee.co.id/"));
+	headers.insert("x-csrftoken", reqwest::header::HeaderValue::from_str(&csrftoken_string)?);
+	headers.insert("af-ac-enc-sz-token", reqwest::header::HeaderValue::from_static(""));
 	headers.insert(reqwest::header::COOKIE, reqwest::header::HeaderValue::from_str(&cookie_content)?);
-	let body = Body::from(body_str);
-	let client = ClientBuilder::new()
-		.gzip(true)
-		.use_rustls_tls() // Use Rustls for HTTPS
-		.build()?;
 
-	// Buat permintaan HTTP POST
-	let response = client
-		.post("https://mall.shopee.co.id/api/v2/voucher_wallet/save_voucher")
-		.header("Content-Type", "application/json")
-		.headers(headers)
-		.body(body)
-		.version(Version::HTTP_2) 
-		.send()
-		.await?;
-	// Check for HTTP status code indicating an error
-	//let http_version = response.version(); 		// disable output features
-	//println!("HTTP Version: {:?}", http_version); // disable output features
-	let text = response.text().await?;	
-	println!("{}", text);
-	
+	println!("");
+	println!("header:{:#?}", headers);
+	let mut attempt_count = 0;
+	let max_attempts = 3; // Ubah angka sesuai kebutuhan Anda
+	loop {
+		let client = ClientBuilder::new()
+			.gzip(true)
+			.use_rustls_tls() // Use Rustls for HTTPS
+			.build()?;
+
+		// Buat permintaan HTTP POST
+		let response = client
+			.post("https://mall.shopee.co.id/api/v2/voucher_wallet/save_voucher")
+			.header("Content-Type", "application/json")
+			.headers(headers.clone())
+			.body(body_str.clone())
+			.version(Version::HTTP_2) 
+			.send()
+			.await?;
+		// Check for HTTP status code indicating an error
+		//let http_version = response.version(); 		// disable output features
+		//println!("HTTP Version: {:?}", http_version); // disable output features
+		let status = response.status();
+		println!("{}", status);
+		let text = response.text().await?;	
+		if status == reqwest::StatusCode::OK {
+			println!("{}", text);
+			break;
+		} else if status == reqwest::StatusCode::IM_A_TEAPOT {
+			println!("Gagal, status code: 418 - I'm a teapot. Mencoba kembali...");
+			println!("{}", text);
+			attempt_count += 1;
+			if attempt_count >= max_attempts {
+				println!("Batas percobaan maksimum tercapai.");
+				break;
+			}
+			continue;
+		}else {
+			println!("Status: {}", status);
+			break;
+		}
+	}
 	Ok(())
 }
 
@@ -132,7 +140,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Check if there are command line arguments
 	
 	println!("-------------------------------------------");
-	println!("save_vouchers [Version 1.0.0]");
+	println!("save_vouchers [Version 1.1.0]");
 	println!("");
 	println!("Dapatkan Info terbaru di https://google.com");
 	println!("");
@@ -140,8 +148,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let opt = Opt::from_args();
     let mut start = String::new();
     let mut end = String::new();
-    
-    // Display the list of available cookie files
+    let mode = select_mode();
+
+	// Display the list of available cookie files
     println!("Daftar file cookie yang tersedia:");
     let files = std::fs::read_dir("./akun")?;
     let mut file_options = Vec::new();
@@ -171,22 +180,61 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file_path = format!("./akun/{}", selected_file);
     let mut cookie_content = String::new();
     File::open(&file_path)?.read_to_string(&mut cookie_content)?;
-	
-	println!("Contoh input: Awal: 12905192072, Akhir: 12905192100");
-    println!("Masukkan nilai start:");
-    io::stdin().read_line(&mut start).expect("Gagal membaca baris");
+    match mode {
+        Mode::Normal => {
+            println!("Contoh input: \npromotion_id: 856793882394624, \nSignature: 8e8a4ced8d6905570114f163a08a15de55c3fed560f8a3a8a25e6e179783b480");
+			println!("Masukkan nilai promotion_id:");
+			io::stdin().read_line(&mut start).expect("Gagal membaca baris");
 
-    println!("Masukkan nilai akhir:");
-    io::stdin().read_line(&mut end).expect("Gagal membaca baris");
-	
-	let task_time_str = opt.time.clone().unwrap_or_else(|| get_user_input("Enter task time (HH:MM:SS.NNNNNNNNN): "));
-    let task_time_dt = parse_task_time(&task_time_str)?;
+			println!("Masukkan nilai Signature:");
+			io::stdin().read_line(&mut end).expect("Gagal membaca baris");
+			
+			let task_time_str = opt.time.clone().unwrap_or_else(|| get_user_input("Enter task time (HH:MM:SS.NNNNNNNNN): "));
+			let task_time_dt = parse_task_time(&task_time_str)?;
+			
+			let endtrim = end.trim();
 
-    // Process HTTP with common function
-	countdown_to_task(&task_time_dt).await;
-    some_function(&start, &end, &cookie_content, &selected_file).await?;
-	
+			// Process HTTP with common function
+			countdown_to_task(&task_time_dt).await;
+			some_function(&start, &endtrim, &cookie_content).await?;
+        }
+        Mode::Collection => {
+            println!("Contoh input: collection_id: 12923214728");
+			println!("Masukkan nilai collection_id:");
+			io::stdin().read_line(&mut start).expect("Gagal membaca baris");
+			let task_time_str = opt.time.clone().unwrap_or_else(|| get_user_input("Enter task time (HH:MM:SS.NNNNNNNNN): "));
+			let task_time_dt = parse_task_time(&task_time_str)?;
+			let starttrim = start.trim();
+			// Process HTTP with common function
+			countdown_to_task(&task_time_dt).await;
+			let (promotion_id, signature) = prepare::some_function(&starttrim, &cookie_content).await?;
+			println!("{}", promotion_id);
+			println!("{}", signature);
+			some_function(&promotion_id, &signature, &cookie_content).await?;
+            // Tambahkan logika untuk mode Teapot di sini
+        }
+    }
     Ok(())
+}
+
+fn select_mode() -> Mode {
+    loop {
+        println!("Pilih mode:");
+        println!("1. Normal");
+        println!("2. Collection");
+
+        print!("Masukkan pilihan (1/2): ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Gagal membaca baris");
+
+        match input.trim() {
+            "1" => return Mode::Normal,
+            "2" => return Mode::Collection,
+            _ => println!("Pilihan tidak valid, coba lagi."),
+        }
+    }
 }
 async fn countdown_to_task(task_time_dt: &NaiveDateTime) {
     loop {
---
+++ src/prepare.rs
@@ -0,0 +1,229 @@
+use reqwest;
+use reqwest::ClientBuilder;
+use reqwest::header::HeaderMap;
+use serde_json;
+use serde_json::Value;
+use serde::Serialize;
+use anyhow::Result;
+use reqwest::Version;
+
+#[derive(Serialize)]
+struct JsonRequest {
+    voucher_collection_request_list: Vec<VoucherCollectionRequest>,
+}
+#[derive(Serialize)]
+struct VoucherCollectionRequest {
+    collection_id: String,
+    component_type: i64,
+    component_id: i64,
+    limit: i64,
+    microsite_id: i64,
+    offset: i64,
+    number_of_vouchers_per_row: i64,
+}
+
+fn extract_csrftoken(cookie_string: &str) -> String {
+    let mut csrftoken = String::new();
+    if let Some(token_index) = cookie_string.find("csrftoken=") {
+        let token_start = token_index + "csrftoken=".len();
+        if let Some(token_end) = cookie_string[token_start..].find(';') {
+            csrftoken = cookie_string[token_start..token_start + token_end].to_string();
+        }
+    }
+    csrftoken
+}
+
+pub async fn some_function(start: &str, cookie_content: &str) -> Result<(String, String)> {
+    let cookie_content_owned = cookie_content.to_string();
+    let csrftoken = extract_csrftoken(&cookie_content_owned);
+    println!("csrftoken: {}", csrftoken);
+	let csrftoken_string = csrftoken.to_string();
+	let voucher_request = VoucherCollectionRequest {
+		collection_id: start.to_string(),
+		component_type: 2,
+		component_id: 1712077200,
+		limit: 100,
+		microsite_id: 63749,
+		offset: 0,
+		number_of_vouchers_per_row: 2,
+	};
+	
+	let mut headers = reqwest::header::HeaderMap::new();
+	headers.insert("User-Agent", reqwest::header::HeaderValue::from_static("Android app Shopee appver=32250 app_type=1"));
+	headers.insert("accept", reqwest::header::HeaderValue::from_static("application/json"));
+	headers.insert("Content-Type", reqwest::header::HeaderValue::from_static("application/json"));
+	headers.insert("x-api-source", reqwest::header::HeaderValue::from_static("rn"));
+	headers.insert("if-none-match-", reqwest::header::HeaderValue::from_static("55b03-8f1a78d495601e3a183dd4c1efb8ac00"));
+	headers.insert("shopee_http_dns_mode", reqwest::header::HeaderValue::from_static("1"));
+	headers.insert("x-shopee-client-timezone", reqwest::header::HeaderValue::from_static("Asia/Jakarta"));
+	headers.insert("af-ac-enc-dat", reqwest::header::HeaderValue::from_static(""));
+	headers.insert("af-ac-enc-id", reqwest::header::HeaderValue::from_static(""));
+	headers.insert("x-sap-access-t", reqwest::header::HeaderValue::from_static(""));
+	headers.insert("x-sap-access-f", reqwest::header::HeaderValue::from_static(""));
+	headers.insert("referer", reqwest::header::HeaderValue::from_static("https://mall.shopee.co.id/"));
+	headers.insert("x-csrftoken", reqwest::header::HeaderValue::from_str(&csrftoken_string)?);
+	headers.insert("af-ac-enc-sz-token", reqwest::header::HeaderValue::from_static(""));
+	headers.insert(reqwest::header::COOKIE, reqwest::header::HeaderValue::from_str(&cookie_content)?);
+
+	// Bentuk struct JsonRequest
+	let json_request = JsonRequest {
+		voucher_collection_request_list: vec![voucher_request],
+	};
+
+	// Convert struct to JSON
+	let json_body = serde_json::to_string(&json_request)?;
+	println!("{}", json_body);
+	
+	loop {
+		let client = ClientBuilder::new()
+			.gzip(true)
+			.use_rustls_tls() // Use Rustls for HTTPS
+			.build()?;
+
+		// Buat permintaan HTTP POST
+		let response = client
+			.post("https://mall.shopee.co.id/api/v1/microsite/get_vouchers_by_collections")
+			.header("Content-Type", "application/json")
+			.headers(headers.clone())
+			.body(json_body.clone())
+			.version(Version::HTTP_2) 
+			.send()
+			.await?;
+		// Check for HTTP status code indicating an error
+		//let http_version = response.version(); 		// disable output features
+		//println!("HTTP Version: {:?}", http_version); // disable output features
+		let status = response.status();
+		let text = response.text().await?;
+		//println!("{}", text);
+		if status == reqwest::StatusCode::OK {
+			let hasil: Value = serde_json::from_str(&text)?;
+			/*let error_res = hasil.get("error").and_then(|er| er.as_i64()).unwrap_or(0);
+			let error_res_str = error_res.to_string();*/
+			// Access specific values using serde_json::Value methods
+			if let Some(data_array) = hasil.get("data").and_then(|data| data.as_array()) {
+				for data_value in data_array {
+					if let Some(vouchers_array) = data_value.get("vouchers").and_then(|vouchers| vouchers.as_array()) {
+						for voucher_value in vouchers_array {
+							if let Some(voucher_obj) = voucher_value.get("voucher").and_then(|voucher| voucher.as_object()) {
+								if let Some(voucher_identifier_obj) = voucher_obj.get("voucher_identifier").and_then(|vi| vi.as_object()) {
+									let promotion_id_temp = voucher_identifier_obj.get("promotion_id").and_then(|pi| pi.as_i64()).unwrap_or(0);
+									let signature_temp = voucher_identifier_obj.get("signature").and_then(|s| s.as_str()).unwrap_or("");
+									let promotion_id = promotion_id_temp.to_string();
+                                    let signature = signature_temp.to_string();
+									/*println!("{}", promotion_id);
+									println!("{}", signature);*/
+                                    return Ok((promotion_id, signature));
+								}
+							}
+						}
+					}else{
+						println!("API Checker 1");
+						let cid_1 = start.to_string();
+						let (promotion_id, signature) = api_1(&cid_1, &headers.clone()).await?;
+						return Ok((promotion_id.to_string(), signature.to_string()));
+					}
+				}
+			/*} else if !error_res_str.is_empty() {
+				interactive_print(&pb, &println!("error: {}", error_res_str));*/
+			}else {
+				println!("Tidak ada data ditemukan untuk collection_id: {}", start.to_string());
+			}
+			break;
+		}else if status == reqwest::StatusCode::IM_A_TEAPOT {
+			println!("POST request gagal untuk collection_id:: {}", start.to_string());
+			println!("Gagal, status code: 418 - I'm a teapot. Mencoba kembali...");
+			println!("{}", text);
+			continue;
+		}else {
+			println!("POST request gagal untuk collection_id:: {}", start.to_string());
+			println!("Status: {}", status);
+			break;
+		}
+	}
+	Ok((String::new(), String::new()))	
+}
+
+async fn api_1(cid_1: &str, headers: &HeaderMap) -> Result<(String, String)> {
+	let cloned_headers = headers.clone();
+	let voucher_request = VoucherCollectionRequest {
+		collection_id: cid_1.to_string(),
+		component_type: 1,
+		component_id: 1708068524282,
+		limit: 100,
+		microsite_id: 62902,
+		offset: 0,
+		number_of_vouchers_per_row: 1,
+	};
+	// Bentuk struct JsonRequest
+	let json_request = JsonRequest {
+		voucher_collection_request_list: vec![voucher_request],
+	};
+
+	// Convert struct to JSON
+	let json_body = serde_json::to_string(&json_request)?;
+	
+	loop {
+		let client = ClientBuilder::new()
+			.gzip(true)
+			.use_rustls_tls() // Use Rustls for HTTPS
+			.build()?;
+
+		// Buat permintaan HTTP POST
+		let response = client
+			.post("https://mall.shopee.co.id/api/v1/microsite/get_vouchers_by_collections")
+			.header("Content-Type", "application/json")
+			.headers(cloned_headers.clone())
+			.body(json_body.clone())
+			.version(Version::HTTP_2) 
+			.send()
+			.await?;
+		// Check for HTTP status code indicating an error
+		//let http_version = response.version(); 		// disable output features
+		//println!("HTTP Version: {:?}", http_version); // disable output features
+		let status = response.status();
+		let text = response.text().await?;
+		if status == reqwest::StatusCode::OK {
+			let hasil: Value = serde_json::from_str(&text)?;
+			/*let error_res = hasil.get("error").and_then(|er| er.as_i64()).unwrap_or(0);
+			let error_res_str = error_res.to_string();*/
+			// Access specific values using serde_json::Value methods
+			if let Some(data_array) = hasil.get("data").and_then(|data| data.as_array()) {
+				for data_value in data_array {
+					if let Some(vouchers_array) = data_value.get("vouchers").and_then(|vouchers| vouchers.as_array()) {
+						for voucher_value in vouchers_array {
+							if let Some(voucher_obj) = voucher_value.get("voucher").and_then(|voucher| voucher.as_object()) {
+								if let Some(voucher_identifier_obj) = voucher_obj.get("voucher_identifier").and_then(|vi| vi.as_object()) {
+									let promotion_id_temp = voucher_identifier_obj.get("promotion_id").and_then(|pi| pi.as_i64()).unwrap_or(0);
+									let signature_temp = voucher_identifier_obj.get("signature").and_then(|s| s.as_str()).unwrap_or("");
+									let promotion_id = promotion_id_temp.to_string();
+                                    let signature = signature_temp.to_string();
+									/*println!("{}", promotion_id);
+									println!("{}", signature);*/
+                                    return Ok((promotion_id, signature));
+								}
+							}
+						}
+					}else{
+						println!("Bug API 2");
+						println!("Tidak ada Info vouchers ditemukan untuk collection_id:{}", cid_1);
+					}
+				}
+			/*} else if !error_res_str.is_empty() {
+				interactive_print(&pb, &println!("error: {}", error_res_str));*/
+			}else {
+				println!("Tidak ada data ditemukan untuk collection_id: {}", cid_1);
+			}
+			break;
+		}else if status == reqwest::StatusCode::IM_A_TEAPOT {
+			println!("POST request gagal untuk collection_id:: {}", cid_1);
+			println!("Gagal, status code: 418 - I'm a teapot. Mencoba kembali...");
+			println!("{}", text);
+			continue;
+		}else {
+			println!("POST request gagal untuk collection_id:: {}", cid_1);
+			println!("Status: {}", status);
+			break;
+		}
+	}
+	Ok((String::new(), String::new()))	
+}
